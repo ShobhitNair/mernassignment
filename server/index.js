d8efc865acddce4a2d7a967bffadce3c4f555984
ShobhitNair <shobhit842160@gmail.com>
2024-02-29 19:52:38 +0530

    images for readme

--- server/index.js
+++ server/index.js
@@ -44,8 +44,8 @@ app.post('/api/subscribe', async (req, res) => {
     res.status(500).json({ message: 'Server error' });
   }
 });
+app.use(express.static(path.resolve(__dirname,"client","build")));
 app.get('/',(req,res)=>{
-  app.use(express.static(path.resolve(__dirname,"client","build")));
   res.sendFile(path.resolve(__dirname,"client","build","index.html"))
 })
 
